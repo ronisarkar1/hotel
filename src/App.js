1784090d073112594cb4abeef76c095266bb5124
swapno <swapno5896@yahoo.com>
2021-05-16 14:56:23 -0700

    D

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import Home from "./components/MainHome/Home/Home";
+// react bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
   return (
     <div>
-      
+      <Home></Home>
     </div>
   );
 };
--- src/components/MainHome/Home/Home.js
+++ src/components/MainHome/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import ChooseUs from '../../ChooseUs/ChooseUs';
+import Footer from '../../Footer/Footer';
+import Review from '../../Review/Review';
+import RoomSuit from '../../RoomSuit/RoomSuit';
 
 const Home = () => {
     return (
         <div>
-            
+            <RoomSuit></RoomSuit>
+            <Review></Review>
+            <ChooseUs></ChooseUs>
+            <Footer></Footer>
         </div>
     );
 };

c762be3bd9b2505b3d552bb4b794e6798bc5348e
swapno <swapno5896@yahoo.com>
2021-05-18 19:17:43 -0700

    D

--- src/components/MainHome/Home/Home.js
+++ src/components/MainHome/Home/Home.js
@@ -1,24 +1,19 @@
 import React from 'react';
-<<<<<<< HEAD
 import Header from '../../Header/Header';
-=======
 import ChooseUs from '../../ChooseUs/ChooseUs';
 import Footer from '../../Footer/Footer';
 import Review from '../../Review/Review';
 import RoomSuit from '../../RoomSuit/RoomSuit';
->>>>>>> 1784090d073112594cb4abeef76c095266bb5124
 
 const Home = () => {
     return (
         <div>
-<<<<<<< HEAD
             <Header></Header>
-=======
             <RoomSuit></RoomSuit>
             <Review></Review>
             <ChooseUs></ChooseUs>
             <Footer></Footer>
->>>>>>> 1784090d073112594cb4abeef76c095266bb5124
+            <h2>fdslkflsd</h2>
         </div>
     );
 };
